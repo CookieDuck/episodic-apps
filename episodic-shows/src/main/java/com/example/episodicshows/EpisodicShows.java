987137e349ce96d11a257cd76d2a880da32c2835
Paul Ganster <paul.ganster@gmail.com>
2017-05-25 08:55:48 -0600

    Added Eureka Awareness to shows and events apps

--- episodic-shows/src/main/java/com/example/episodicshows/EpisodicShows.java
+++ episodic-shows/src/main/java/com/example/episodicshows/EpisodicShows.java
@@ -2,11 +2,13 @@ package com.example.episodicshows;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-public class DemoApplication {
+@EnableDiscoveryClient
+public class EpisodicShows {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(EpisodicShows.class, args);
 	}
 }
